232b9a162ee5dc5c0c3dfeaea03c5b036ee86722
stivencabrera3 <stiven.cabrera3@gmail.com>
2024-05-22 15:53:43 -0400

    add examples of lifecycle methods

--- springcoredemo/src/main/java/com/love2code/springboot/springcoredemo/common/BaseballCoach.java
+++ springcoredemo/src/main/java/com/love2code/springboot/springcoredemo/common/BaseballCoach.java
@@ -1,5 +1,7 @@
 package com.love2code.springboot.springcoredemo.common;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
@@ -10,5 +12,28 @@ public class BaseballCoach implements Coach {
     @Override
     public String getDailyWorkout() {
         return "hit some balls with the stick. thats it.";
+    }
+
+    /*
+    *   Lifecycle methods
+    *
+    * @PostConstruct is used to define a method that should be called immediately after the bean's initialization
+    *
+    * you can see the sout in the log when the application starts
+    *
+    * @PreDestroy is used to define a method that should be called immediately before the bean is destroyed
+    *
+    * when you stop the application, you will see the sout in the log just before the application stops
+    *
+    * for prototype scoped beans, spring doesnt call the @PreDestroy method because id does not manage the complete lifecycle of a prototype bean
+    * */
+    @PostConstruct
+    public void init() {
+        System.out.println("BaseballCoach: inside init method");
+    }
+
+    @PreDestroy
+    public void destroy() {
+        System.out.println("BaseballCoach: inside destroy method");
     }
 }
