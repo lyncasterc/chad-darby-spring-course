907327a35ab4e46a3c9c9e46db49a94114c1461c
stivencabrera3 <stiven.cabrera3@gmail.com>
2024-05-21 23:06:24 -0400

    complete section 1, start section 2

--- 01-spring-boot-overview/01-spring-boot-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ 01-spring-boot-overview/01-spring-boot-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,5 +1,6 @@
 package com.luv2code.springboot.demo.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +16,15 @@ public class FunRestController {
     @GetMapping("/workout")
     public String getWorkout() {
         return "<h2>Run! Harder!</h2>";
+    }
+
+    // using custom properties defined in application.properties
+
+    @Value("${developer.name}")
+    private String developerName;
+
+    @GetMapping("/devname")
+    public String getDeveloperName() {
+        return developerName;
     }
 }
