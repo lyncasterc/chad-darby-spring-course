b21b5897d0d2645a97661c88eb33b0a6f8b6489e
stivencabrera3 <stiven.cabrera3@gmail.com>
2024-05-22 13:10:40 -0400

    Add dependency injection example through constructor injection

---
+++ springcoredemo/src/main/java/com/love2code/springboot/springcoredemo/Coach.java
@@ -0,0 +1,5 @@
+package com.love2code.springboot.springcoredemo;
+
+public interface Coach {
+    String getDailyWorkout();
+}
---
+++ springcoredemo/src/main/java/com/love2code/springboot/springcoredemo/CricketCoach.java
@@ -0,0 +1,14 @@
+package com.love2code.springboot.springcoredemo;
+
+import org.springframework.stereotype.Component;
+
+// this annotation marks class as a Spring Bean
+// spring will create an instance of the class and inject it
+// and now spring can inject this as a dependency
+@Component
+public class CricketCoach implements Coach{
+    @Override
+    public String getDailyWorkout() {
+        return "Practice some cricket stuff idk";
+    }
+}
