6ae287dcb18d818577fc3b36d21a19f92e033dcb
stivencabrera3 <stiven.cabrera3@gmail.com>
2024-05-24 18:49:59 -0400

    Add StudentRestController

---
+++ restcrud/src/main/java/com/love2code/restcrud/rest/StudentRestController.java
@@ -0,0 +1,48 @@
+package com.love2code.restcrud.rest;
+
+import com.love2code.restcrud.entity.Student;
+import jakarta.annotation.PostConstruct;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/students")
+public class StudentRestController {
+    // hard coded for now
+    private HashMap<Integer, Student> studentHashMap;
+
+    @PostConstruct
+    private void populateMockData() {
+        this.studentHashMap = new HashMap<>();
+
+        this.studentHashMap.put(0, new Student("John", "Doe"));
+        this.studentHashMap.put(1, new Student("Jane", "Dane"));
+        this.studentHashMap.put(2, new Student("Bobby", "Robby"));
+    }
+
+    @GetMapping
+    private ResponseEntity<List<Student>> getStudents() {
+
+        List<Student> students = new ArrayList<>(this.studentHashMap.values());
+
+        return ResponseEntity.ok(students);
+    }
+
+    @GetMapping("/{studentId}")
+    private ResponseEntity<Student> getStudentById(@PathVariable int studentId) {
+        Student student = this.studentHashMap.get(studentId);
+
+        if (student == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(student);
+    }
+}
