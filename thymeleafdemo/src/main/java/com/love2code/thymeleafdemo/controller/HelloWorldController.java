e76de08832b51719fb2eae9ba5211e07782772b4
stivencabrera3 <stiven.cabrera3@gmail.com>
2024-05-29 14:07:47 -0400

    Add example of request param binding

--- thymeleafdemo/src/main/java/com/love2code/thymeleafdemo/controller/HelloWorldController.java
+++ thymeleafdemo/src/main/java/com/love2code/thymeleafdemo/controller/HelloWorldController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -14,11 +15,25 @@ public class HelloWorldController {
         return "helloworld-form";
     }
 
-    @GetMapping("/processForm")
+    // example getting request param from HttpServetRequest
+
+    /*@GetMapping("/processForm")
     private String processForm(HttpServletRequest request, Model model) {
 
         // gets url params from the request
         String studentName = request.getParameter("studentName");
+        String message = studentName.isEmpty() ? "ya dirty dog" : studentName + ", ya dirty dog";
+
+        model.addAttribute("message", message);
+
+        return "helloworld";
+    }*/
+
+    // example of using request param binding using @RequestParam
+
+    @GetMapping("/processForm")
+    private String processForm(@RequestParam("studentName") String studentName, Model model) {
+
         String message = studentName.isEmpty() ? "ya dirty dog" : studentName + ", ya dirty dog";
 
         model.addAttribute("message", message);
