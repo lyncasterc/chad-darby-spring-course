d92a4664455fad33e97c4a7b693855f0e6f7ee66
stivencabrera3 <stiven.cabrera3@gmail.com>
2024-05-30 23:36:45 -0400

    Add initbinder example

--- thymeleafdemo/src/main/java/com/love2code/thymeleafdemo/controller/StudentController.java
+++ thymeleafdemo/src/main/java/com/love2code/thymeleafdemo/controller/StudentController.java
@@ -3,10 +3,13 @@ package com.love2code.thymeleafdemo.controller;
 import com.love2code.thymeleafdemo.model.Student;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -14,6 +17,14 @@ import java.util.List;
 
 @Controller
 public class StudentController {
+
+    // initbinder methods run before any request handler methods do
+    @InitBinder private void initBinder(WebDataBinder dataBinder) {
+        StringTrimmerEditor stringTrimmerEditor= new StringTrimmerEditor(true);
+
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
+
     @Value("${countries}")
     private List<String> countries;
 
@@ -33,6 +44,8 @@ public class StudentController {
         return "student-form";
     }
 
+    //@Valid annotation tells Spring MVC to validate fields against any constraints
+    // BindingResult will contain any errors
     @PostMapping("/studentForm")
     private String processStudentForm(@Valid @ModelAttribute("student") Student student, BindingResult bindingResult, Model model) {
 
